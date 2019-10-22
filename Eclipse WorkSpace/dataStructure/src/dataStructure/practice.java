5a9de6486532f102395035fc918ec733926302de
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-22 19:57:01 +0300

    ok yall

---
+++ Eclipse WorkSpace/dataStructure/src/dataStructure/practice.java
@@ -0,0 +1,39 @@
+package dataStructure;
+
+import java.util.*;
+
+public class practice {
+
+	public static void main(String[] args) {
+		ArrayList<String> uusiTaulu = new ArrayList<String>();
+		uusiTaulu.add("eka");
+		uusiTaulu.add("toka");
+		uusiTaulu.add("kolmas");
+
+		for(int i = 0; i < uusiTaulu.size(); i++) {
+			System.out.println(uusiTaulu.get(i));
+		}
+
+		TreeSet<String> fruits = new TreeSet<String>();
+
+		fruits.add("Eka");
+		fruits.add("Toka");
+		fruits.add("Kolmas");
+
+		Iterator<String> itr = fruits.iterator();
+
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+		HashMap<String, String> parit = new HashMap<>();
+		parit.put("koira", "dog");
+		parit.put("kissa","cat");
+
+		for (String string : parit.keySet()) {
+			System.out.println(string +  " --> " + parit.get(string));
+		}
+
+	}
+
+}
