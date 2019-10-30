e5ae68724b90c624e06b48b8c4499d29578c7a80
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-30 18:42:00 +0200

    ok

---
+++ Eclipse WorkSpace Laptop/tietokanta/src/tietokanta/demo.java
@@ -0,0 +1,41 @@
+package tietokanta;
+
+import java.sql.*;
+
+class MysqlCon {
+	public static void main(String args[]) {
+	
+		// Varaudutaan poikkeuksiin; esim. jos tietokantaan ei saada yhteytt‰
+		try {
+			// M‰‰ritell‰‰n tietokannan yhteysosoite sek‰ tietokannan nimi
+			String URL = "jdbc:mysql://localhost:3306/database";
+			// M‰‰ritell‰‰n k‰ytt‰j‰tiedot yhteyden luomista varten
+			String USERID = "root";
+			String PASSWORD = "";
+			
+			// Luodaan yhteys k‰ytt‰en edell‰nmainittuja tietoja
+			Connection con = DriverManager.getConnection(URL, USERID, PASSWORD);
+			
+			System.out.println("Yhteys tietokantaan on luotu.");
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Artist");
+			while (rs.next()){
+				System.out.println(rs.getString(1) + "  " + rs.getString(2));
+			}
+			
+			ResultSet rs1 = stmt.executeQuery("SELECT * FROM Album");
+			while (rs1.next()) {
+				System.out.println(rs1.getString(1) + "  " + rs1.getString(2));
+			}
+
+		
+			// Suljetaan yhteys
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println("Virhe tietokannan k‰ytˆss‰!");
+			System.out.println(e);
+		} // catch
+	} // main
+} // class
