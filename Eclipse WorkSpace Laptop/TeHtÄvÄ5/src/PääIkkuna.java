eecbb95bbfe5122e1bf19806df23e06605be83d6
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-11-13 21:02:46 +0200

    More work

--- Eclipse WorkSpace Laptop/TeHtÄvÄ5/src/PääIkkuna.java
+++ Eclipse WorkSpace Laptop/TeHtÄvÄ5/src/PääIkkuna.java
@@ -8,6 +8,13 @@ import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import database.Connection;
+import database.Exception;
+import database.Object;
+import database.ResultSet;
+import database.Statement;
+import database.String;
+
 import java.awt.event.*;
 import java.sql.*;
 
@@ -45,22 +52,26 @@ public class P‰‰Ikkuna extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		String [] KolumnienNimet = {"Pelaajan nimi", "Pelaajan pelipaikka", "Pelaajan pelinumero"};
-		DefaultTableModel model = new DefaultTableModel(KolumnienNimet, 0);
-		contentPane.setLayout(null);
-		JTable table = new JTable(model);
-		table.setBorder(null);
-		table.setDefaultEditor(Object.class, null);
-		table.setBounds(0, 0, 388, 295);
-		JScrollPane sp = new JScrollPane(table);
-		sp.setBorder(null);
-		sp.setBounds(0, 0, 614, 209);
-		contentPane.add(sp);
+		LuoTaulu();
+	
 		
 		JButton btnNewButton = new JButton("Lis\u00E4ys ");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				  try {			 
+				Lis‰ysIkkuna Aa = new Lis‰ysIkkuna();
+				Aa.setVisible(true);
+				System.out.println(Aa.getPPaikka());
+				LuoTaulu();
+
+			}		
+		});			 
+		btnNewButton.setBounds(29, 251, 135, 70);
+		contentPane.add(btnNewButton);
+		
+		JButton btnPoisto = new JButton("Poisto");
+		btnPoisto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 try {			 
 					  String URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7311675";
 					  String USERID = "sql7311675";
 					  String PASSWORD = "rcv4C45n1M";
@@ -75,16 +86,57 @@ public class P‰‰Ikkuna extends JFrame {
 				  } // catch
 
 			}		
-		});			 
-		btnNewButton.setBounds(29, 251, 135, 70);
-		contentPane.add(btnNewButton);
-		
-		JButton btnPoisto = new JButton("Poisto");
+		});
 		btnPoisto.setBounds(243, 251, 135, 70);
 		contentPane.add(btnPoisto);
 		
 		JButton btnPivitys = new JButton("P\u00E4ivitys");
+		btnPivitys.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LuoTaulu();
+			}
+		});
 		btnPivitys.setBounds(445, 251, 135, 70);
 		contentPane.add(btnPivitys);
+	}
+	
+	public void LuoTaulu() {
+		String [] KolumnienNimet = {"Pelaajan nimi", "Pelaajan pelipaikka", "Pelaajan pelinumero"};
+		DefaultTableModel model = new DefaultTableModel(KolumnienNimet, 0);
+		contentPane.setLayout(null);
+		JTable table = new JTable(model);
+		table.setBorder(null);
+		table.setDefaultEditor(Object.class, null);
+		table.setBounds(0, 0, 388, 295);
+		JScrollPane sp = new JScrollPane(table);
+		sp.setBorder(null);
+		sp.setBounds(0, 0, 614, 209);
+		contentPane.add(sp);
+		
+		  String SQL = "SELECT * FROM Pelaajat";
+		  System.out.print(SQL);
+		  updateTable(SQL, model, table);
+	}
+
+	private void updateTable(String SQL, DefaultTableModel model, JTable table) {
+		// TODO Auto-generated method stub
+		 try {			 
+			  String URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7311675";
+			  String USERID = "sql7311675";
+			  String PASSWORD = "rcv4C45n1M";
+			
+			  Connection con = DriverManager.getConnection(URL, USERID, PASSWORD);
+			  System.out.println("Yhteys tietokantaan on luotu.");
+			  Statement stmt = con.createStatement();
+			  ResultSet rs = stmt.executeQuery(SQL);
+			  
+			  while (rs.next()) {
+				  System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+				  	model.addRow(new Object[] {rs.getString(1),rs.getString(2),rs.getString(3)});
+			  }
+		 }
+		 catch(Exception e) {
+			 e.printStackTrace();
+		 }
 	}
 }
